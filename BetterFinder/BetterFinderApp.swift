1b16fe917edd5588be36725ad516b6a22726aff1
Francesco Albano <francesco.albano@studenti.unina.it>
2026-03-26 20:34:35 +0100

    Initial Commit

---
+++ BetterFinder/BetterFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterFinderApp.swift
+//  BetterFinder
+//
+//  Created by Francesco Albano on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterFinderTests/BetterFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterFinderTests.swift
+//  BetterFinderTests
+//
+//  Created by Francesco Albano on 26/03/26.
+//
+
+import Testing
+@testable import BetterFinder
+
+struct BetterFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
